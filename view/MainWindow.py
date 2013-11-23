1ac35c6202238813935e750718c4570c2928cd8e
Cecilia <cecilia.lejeu@gmail.com>
2013-11-23 22:39:44 +0100

    MenuBar added to the MainWindow

--- view/MainWindow.py
+++ view/MainWindow.py
@@ -12,6 +12,9 @@ class MainWindow(QtGui.QMainWindow):
         self._controller = controller
         self.setWindowTitle('EWP')
 
+        #Actions
+
+
         #Main Widgets
         self._sessionView = None
 
@@ -21,6 +24,8 @@ class MainWindow(QtGui.QMainWindow):
         self.restoreGeometry(geometry)
 
         self.loadStyleSheet()
+        self.createAction()
+        self.initMenu()
         self.initView()
 
     def loadStyleSheet(self, styleFile=None):
@@ -71,6 +76,40 @@ class MainWindow(QtGui.QMainWindow):
         self.addDockWidget(QtCore.Qt.RightDockWidgetArea, rightDockWidget)
 
 
+    def initMenu(self):
+        """
+        Create the main menu.
+        """
+        self.fileMenu = self.menuBar().addMenu(self.tr("&File"))
+        self.fileMenu.addAction(self.createProjectAction)
+        self.fileMenu.addAction(self.openProjectAction)
+        self.fileMenu.addAction(self.saveProjectAction)
+
+        self.helpMenu = self.menuBar().addMenu(self.tr("&Help"))
+        self.helpMenu.addAction(self.aboutAction)
+
+    def createAction(self):
+        """
+        Set up all the actions usefull in the main window
+        """
+        self.createProjectAction = QtGui.QAction(self.tr("&New Project"), self)
+        self.createProjectAction.setShortcut(QtGui.QKeySequence.New)
+        self.setStatusTip(self.tr("Create a new project"))
+        self.connect(self.createProjectAction, QtCore.SIGNAL("triggered()"), self, QtCore.SLOT("newProject()"))
+
+        self.openProjectAction = QtGui.QAction(self.tr("&Open..."), self)
+        self.openProjectAction.setShortcut(QtGui.QKeySequence.Open)
+        self.setStatusTip(self.tr("Open an existing project"))
+        self.connect(self.openProjectAction, QtCore.SIGNAL("triggered()"), self, QtCore.SLOT("openProject()"))
+
+        self.saveProjectAction = QtGui.QAction(self.tr("&Save"), self)
+        self.saveProjectAction.setShortcut(QtGui.QKeySequence.Save)
+        self.setStatusTip(self.tr("Save the current project"))
+        self.connect(self.saveProjectAction, QtCore.SIGNAL("triggered()"), self, QtCore.SLOT("save()"))
+
+        self.aboutAction = QtGui.QAction(self.tr("&About"), self)
+        self.setStatusTip(self.tr("Show the credits and authors"))
+        self.connect(self.aboutAction, QtCore.SIGNAL("triggered()"), self, QtCore.SLOT("showAbout()"))
 
 
     def closeEvent(self, event):
@@ -78,6 +117,29 @@ class MainWindow(QtGui.QMainWindow):
         settings = QtCore.QSettings("Cecilia", "EWP")
         settings.setValue("geometry", self.saveGeometry())
         super(MainWindow, self).closeEvent(event)
+
+    # ------------------------ SIGNAL / SLOTS HANDLER ---------------- #
+
+    def newProject(self):
+        """ Forward the creation of a new project to the controller. """
+        #TODO
+        pass
+
+    def openProject(self):
+        """ Forward the importation of a new project to the controller. """
+        #TODO
+        pass
+
+    def showAbout(self):
+        """ Show a popup window with authors and credits """
+        #TODO
+        pass
+
+    def save(self):
+        """ Save the current project in its state """
+        #TODO
+        pass
+
 
     #------------------------ GETTER / SETTER ------------------------ #
 
